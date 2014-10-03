57ba4f9415c8b74c5bbdb11ee8cd4d36a6cb1065
mxro <mxro@nowhere.com>
2014-10-03 13:32:11 +1000

    Improving QUnit runner Autocommit Fri Oct 03 13:32:11 EST 2014

--- src/test/java/com/appjangle/qunit/tests/TestQUnitEmbedding.java
+++ src/test/java/com/appjangle/qunit/tests/TestQUnitEmbedding.java
@@ -1,15 +1,12 @@
 package com.appjangle.qunit.tests;
 
-import org.junit.Test;
-
 import com.appjangle.qunit.QUnitHtmlUnit;
 
 public class TestQUnitEmbedding {
 
-	@Test(expected = RuntimeException.class)
 	public void test_simple_tests() {
 		QUnitHtmlUnit
-				.runQUnitTest("file:///M:/Git/qunit-htmlunit-java/src/main/resources/qunit_example.html");
+				.runQUnitTest(getClass().getResource("/qunit_example.html").getFile());
 	}
 
 }
