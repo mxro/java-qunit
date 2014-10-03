06229541332c9f3bbb232284cfd0c34780b56fc9
mxro <mxro@nowhere.com>
2014-10-03 13:34:11 +1000

    Improving QUnit runner Autocommit Fri Oct 03 13:34:11 EST 2014

--- src/test/java/com/appjangle/qunit/tests/TestQUnitEmbedding.java
+++ src/test/java/com/appjangle/qunit/tests/TestQUnitEmbedding.java
@@ -1,5 +1,7 @@
 package com.appjangle.qunit.tests;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import com.appjangle.qunit.QUnitHtmlUnit;
@@ -8,8 +10,17 @@ public class TestQUnitEmbedding {
 
 	@Test
 	public void test_simple_tests() {
+		try {
 		QUnitHtmlUnit
 				.runQUnitTest("file:///"+getClass().getResource("/qunit_example.html").getFile());
+		
+		Assert.fail("Test test should have failed since it contained an error.");
+		
+		} catch (Throwable t) {
+			
+			Assert.assertTrue(condition);
+			
+		}
 	}
 
 }
