e8e77505921f36d763c8f70b804d3aac7ad05037
mxro <mxro@nowhere.com>
2014-10-07 11:04:21 +1100

    a

---
+++ src/test/java/com/appjangle/qunit/tests/TestSslScript.java
@@ -0,0 +1,25 @@
+package com.appjangle.qunit.tests;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+
+import com.appjangle.qunit.internal.SslUtils;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+
+public class TestSslScript {
+
+    @Test
+    public void test() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+        final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_24);
+
+        SslUtils.allowInsecureSsl(webClient);
+        SslUtils.disableSslCertificateValidation();
+
+        webClient.getPage("https://appjangle.com/notyet");
+    }
+
+}
